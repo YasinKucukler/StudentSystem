374a47a73e0703d730ccf170d4a025268ca29ca1
YasinKucukler <yasinkucukler48@gmail.com>
2023-12-31 22:24:28 +0300

    update completed now working on delete opt

---
+++ cruddemo/src/main/java/com/yasin/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,18 @@
+package com.yasin.cruddemo.dao;
+
+import com.yasin.cruddemo.entity.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String theLastName);
+
+    void update(Student theStudent);
+}
